4b14e44eb4b2ea255d6b0398278277a94bbe0d63
moonjuicy <122168068+moonjuicy@users.noreply.github.com>
2023-11-28 12:42:57 +0900

    kakao login setup & redirect to root page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,8 @@ import { PrismaClient } from "@prisma/client";
 
 import GoogleProvider from "next-auth/providers/google";
 import NaverProvider from "next-auth/providers/naver";
+import KakaoProvider from "next-auth/providers/kakao";
+
 const prisma = new PrismaClient();
 
 export default NextAuth({
@@ -13,6 +15,10 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID || "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
     }),
     NaverProvider({
       clientId: process.env.NAVER_CLIENT_ID || "",
